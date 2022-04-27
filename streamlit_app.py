7f17f6bfcf451ed0b30d95f659c9bf5916cb17e1
Sahil Kumar <sahil.saini526@gmail.com>
2022-04-27 12:02:13 +0800

    fruit pick option

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,6 +10,9 @@ streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 # importing CSV file from s3 bucket
-
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
+#Pick function
+streamlit.multiselect("Pick some fruits: ", list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
