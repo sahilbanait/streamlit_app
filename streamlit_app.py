2185399452b3a1373959e5ea6265291c720e34fb
Sahil Kumar <sahil.saini526@gmail.com>
2022-05-10 14:59:25 +0800

    Filter the table data

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,21 +18,15 @@ fruits_to_show = my_fruit_list.loc[fruit_selected]
 # Pick function
 streamlit.dataframe(fruits_to_show)
 
-
-
 # New section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','kiwi')
+fruit_choice = streamlit.text_input('What fruit would you like information about?', 'kiwi')
 streamlit.write('The user entered', fruit_choice)
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon"+fruit_choice)
-#take the json version of the response and normalize it
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+# take the json version of the response and normalize it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 
-#output it the screen as a table
+# output it the screen as a table
 streamlit.dataframe(fruityvice_normalized)
-
-
-
-
-
