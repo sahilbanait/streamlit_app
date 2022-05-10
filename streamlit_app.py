44d0915898383d4071904b1e3b3c1b6c78693bc9
Sahil Kumar <sahil.saini526@gmail.com>
2022-05-10 14:48:23 +0800

    Filter the table data

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,13 +13,12 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 # importing CSV file from s3 bucket
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
-fruit_selected = streamlit.multiselect("Pick some fruits: ", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruit_selected = streamlit.multiselect("Pick some fruits: ", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruit_selected]
 
 # New section to display fruityvice api response
-fruityvice_response = requests.get("https://fruitvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 
-
-#Pick function
+# Pick function
 
 streamlit.dataframe(fruits_to_show)
