eeb64d293f6324b2637966b92a9a336bccbcb51a
Sahil Kumar <sahil.saini526@gmail.com>
2022-05-12 10:05:45 +0800

    Filter the table data

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,6 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-
 # importing CSV file from s3 bucket
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
