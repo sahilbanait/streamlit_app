3ed3d73adea41e28df78d5bad2e2e910252ee016
Sahil Kumar <sahil.saini526@gmail.com>
2022-05-14 10:54:42 +0800

    Filter the table data

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,8 +57,7 @@ def get_fruit_load_list():
 if streamlit.button('Get Fruit Load List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_rows = get_fruit_load_list()
-
-streamlit.dataframe(my_data_rows)
+    streamlit.dataframe(my_data_rows)
 
 
 # Allow the end user to add a fruit to the list
@@ -72,4 +71,4 @@ add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 if streamlit.button('Add a fruit to the List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     back_from_function = insert_row_snowflake(add_my_fruit)
-streamlit.text(back_from_function)
+    streamlit.text(back_from_function)
