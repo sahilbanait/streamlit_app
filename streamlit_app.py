ae5e27ad81ec8980e0c9158d2dcd8a26bde15620
Sahil Kumar <sahil.saini526@gmail.com>
2022-05-14 10:07:06 +0800

    Filter the table data

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,6 @@ import pandas
 import requests
 import streamlit
 
-
-
-
-
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
@@ -34,6 +30,7 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
 import snowflake.connector
+
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * from fruit_load_list")
@@ -41,5 +38,6 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains")
 streamlit.dataframe(my_data_rows)
 
-add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
+add_my_fruit = streamlit.text_input('What fruit would you like to add?', 'jackfruit')
 streamlit.write('Thanks for adding jackfruit', add_my_fruit)
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
