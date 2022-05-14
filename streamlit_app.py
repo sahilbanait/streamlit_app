b8bc97882d6490da41b0f4678b655b0022349659
Sahil Kumar <sahil.saini526@gmail.com>
2022-05-14 11:04:32 +0800

    Filter the table data

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,7 +43,7 @@ except URLError as e:
 streamlit.write('The user entered', fruit_choice)
 
 
-streamlit.header("The fruit load list contains")
+streamlit.header("View Our Fruit List - Add Your Favourites")
 
 
 # Snowflake- related functions
@@ -54,7 +54,7 @@ def get_fruit_load_list():
 
 
 # Add a button to load the fruit
-if streamlit.button('Get Fruit Load List'):
+if streamlit.button('Get Fruit List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_rows = get_fruit_load_list()
     my_cnx.close()
